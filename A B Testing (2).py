57ed6961364e81acbb386b0e1b24850133c9e303
Bodapati Bhavya Sree <113964720+Bhavyasree2003@users.noreply.github.com>
2024-03-08 21:33:28 +0530

    Add files via upload

---
+++ A B Testing (2).py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+get_ipython().system('pip install plotly')
+
+
+# In[4]:
+
+
+import pandas as pd
+import datetime
+from datetime import date, timedelta
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+pio.templates.default = "plotly_white"
+
+control_data = pd.read_csv("control_group.csv", sep = ";")
+test_data = pd.read_csv("test_group.csv", sep = ";")
+
+
+# In[5]:
+
+
+print(control_data.head())
+
+
+# In[6]:
+
+
+print(test_data.head())
+
+
+# In[8]:
+
+
+control_data.columns = ["Campaign Name", "Date", "Amount Spent", 
+                        "Number of Impressions", "Reach", "Website Clicks", 
+                        "Searches Received", "Content Viewed", "Added to Cart",
+                        "Purchases"]
+
+test_data.columns = ["Campaign Name", "Date", "Amount Spent", 
+                        "Number of Impressions", "Reach", "Website Clicks", 
+                        "Searches Received", "Content Viewed", "Added to Cart",
+                        "Purchases"]
+
+
+# In[9]:
+
+
+print(control_data.isnull().sum())
+
+
+# In[28]:
+
+
+print(test_data.isnull().sum())
+
+
+# In[11]:
+
+
+control_data["Number of Impressions"].fillna(value=control_data["Number of Impressions"].mean(), 
+                                             inplace=True)
+control_data["Reach"].fillna(value=control_data["Reach"].mean(), 
+                             inplace=True)
+control_data["Website Clicks"].fillna(value=control_data["Website Clicks"].mean(), 
+                                      inplace=True)
+control_data["Searches Received"].fillna(value=control_data["Searches Received"].mean(), 
+                                         inplace=True)
+control_data["Content Viewed"].fillna(value=control_data["Content Viewed"].mean(), 
+                                      inplace=True)
+control_data["Added to Cart"].fillna(value=control_data["Added to Cart"].mean(), 
+                                     inplace=True)
+control_data["Purchases"].fillna(value=control_data["Purchases"].mean(), 
+                                 inplace=True)
+
+
+# In[12]:
+
+
+ab_data = control_data.merge(test_data, 
+                             how="outer").sort_values(["Date"])
+ab_data = ab_data.reset_index(drop=True)
+print(ab_data.head())
+
+
+# In[13]:
+
+
+print(ab_data["Campaign Name"].value_counts())
+
+
+# In[14]:
+
+
+figure = px.scatter(data_frame = ab_data, 
+                    x="Number of Impressions",
+                    y="Amount Spent", 
+                    size="Amount Spent", 
+                    color= "Campaign Name", 
+                    trendline="ols")
+figure.show()
+
+
+# In[15]:
+
+
+label = ["Total Searches from Control Campaign", 
+         "Total Searches from Test Campaign"]
+counts = [sum(control_data["Searches Received"]), 
+          sum(test_data["Searches Received"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Searches')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[16]:
+
+
+label = ["Website Clicks from Control Campaign", 
+         "Website Clicks from Test Campaign"]
+counts = [sum(control_data["Website Clicks"]), 
+          sum(test_data["Website Clicks"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Website Clicks')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[17]:
+
+
+label = ["Content Viewed from Control Campaign", 
+         "Content Viewed from Test Campaign"]
+counts = [sum(control_data["Content Viewed"]), 
+          sum(test_data["Content Viewed"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Content Viewed')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[18]:
+
+
+label = ["Products Added to Cart from Control Campaign", 
+         "Products Added to Cart from Test Campaign"]
+counts = [sum(control_data["Added to Cart"]), 
+          sum(test_data["Added to Cart"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Added to Cart')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[19]:
+
+
+label = ["Amount Spent in Control Campaign", 
+         "Amount Spent in Test Campaign"]
+counts = [sum(control_data["Amount Spent"]), 
+          sum(test_data["Amount Spent"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Amount Spent')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[20]:
+
+
+label = ["Purchases Made by Control Campaign", 
+         "Purchases Made by Test Campaign"]
+counts = [sum(control_data["Purchases"]), 
+          sum(test_data["Purchases"])]
+colors = ['gold','lightgreen']
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Control Vs Test: Purchases')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+
+# In[21]:
+
+
+figure = px.scatter(data_frame = ab_data, 
+                    x="Content Viewed",
+                    y="Website Clicks", 
+                    size="Website Clicks", 
+                    color= "Campaign Name", 
+                    trendline="ols")
+figure.show()
+
+
+# In[22]:
+
+
+figure = px.scatter(data_frame = ab_data, 
+                    x="Added to Cart",
+                    y="Content Viewed", 
+                    size="Added to Cart", 
+                    color= "Campaign Name", 
+                    trendline="ols")
+figure.show()
+
+
+# In[23]:
+
+
+figure = px.scatter(data_frame = ab_data, 
+                    x="Purchases",
+                    y="Added to Cart", 
+                    size="Purchases", 
+                    color= "Campaign Name", 
+                    trendline="ols")
+figure.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
